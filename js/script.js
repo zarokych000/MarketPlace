db96daed997bae0865bcd88748ab4b506e0f4040
Ruslan <rusvolk97i@gmail.com>
2021-03-18 21:19:25 +0300

    adding responsive to main section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log('js');
+"use strict";
+
+function mobileLinkMenu(arrowSelector, menuSelector) {
+  const linkArrow = document.querySelector(arrowSelector);
+  const menu = document.querySelector(menuSelector);
+
+  linkArrow.addEventListener('click', (e) => { 
+    if (e.target.classList.contains('top')){
+      menu.style.cssText = '';
+      linkArrow.classList.remove('top');
+    } else {
+      menu.style.cssText = `overflow: none; height: 100%; border: 4px solid #4668aa`;
+      linkArrow.classList.add('top');
+    }
+  });
+
+
+}
+
+mobileLinkMenu('.link__mobile-arrow', ".link--header");
