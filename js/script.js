00314eb9df28c2334bdbd8af92be3dc1bc30b6c6
Ruslan <rusvolk97i@gmail.com>
2021-03-21 21:12:03 +0300

    finished layout

--- js/script.js
+++ js/script.js
@@ -3,16 +3,17 @@
 function mobileLinkMenu(arrowSelector, menuSelector) {
   const linkArrow = document.querySelector(arrowSelector);
   const menu = document.querySelector(menuSelector);
-
-  linkArrow.addEventListener('click', (e) => { 
-    if (e.target.classList.contains('top')){
-      menu.style.cssText = '';
-      linkArrow.classList.remove('top');
-    } else {
-      menu.style.cssText = `overflow: none; height: 100%; border: 4px solid #4668aa`;
-      linkArrow.classList.add('top');
-    }
-  });
+  if (linkArrow) {
+    linkArrow.addEventListener('click', (e) => { 
+      if (e.target.classList.contains('top')){
+        menu.style.cssText = '';
+        linkArrow.classList.remove('top');
+      } else {
+        menu.style.cssText = `overflow: none; height: 100%; border: 4px solid #4668aa`;
+        linkArrow.classList.add('top');
+      }
+    });
+  }
 
 
 }
